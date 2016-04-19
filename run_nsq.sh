bdc00edc46f5c373028cd02e2b7e6ae6a4f8e6f6
Øyvind Skaar <me@odots.org>
2016-04-19 12:28:27 +0200

    run script changes

--- run_nsq.sh
+++ run_nsq.sh
@@ -1,4 +1,4 @@
-
-konsole --nofork --geometry=1200x300-0+0    -e bash -c './nsq-bin/nsqlookupd; sleep 5'                                     2>/dev/null &
-konsole --nofork --geometry=1200x300-0+350  -e bash -c './nsq-bin/nsqd --lookupd-tcp-address=127.0.0.1:4160; sleep 5'      2>/dev/null &
-konsole --nofork --geometry=1200x300-0+690  -e bash -c './nsq-bin/nsqadmin --lookupd-http-address=127.0.0.1:4161; sleep 5' 2>/dev/null &
+cd _nsq-files
+konsole --nofork --geometry=1200x300-0+0    -e bash -c '../nsq-bin/nsqlookupd; sleep 5'                                     2>/dev/null &
+konsole --nofork --geometry=1200x300-0+350  -e bash -c '../nsq-bin/nsqd --lookupd-tcp-address=127.0.0.1:4160; sleep 5'      2>/dev/null &
+konsole --nofork --geometry=1200x300-0+690  -e bash -c '../nsq-bin/nsqadmin --lookupd-http-address=127.0.0.1:4161; sleep 5' 2>/dev/null &
