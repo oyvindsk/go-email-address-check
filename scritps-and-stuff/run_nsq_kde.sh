57f9585ebac7057ca5f98fb256ed251819473a13
Øyvind Skaar <me@odots.org>
2016-04-21 14:41:59 +0200

    Cleanup

---
+++ scritps-and-stuff/run_nsq_kde.sh
@@ -0,0 +1,4 @@
+cd _nsq-files
+konsole --nofork --geometry=1200x300-0+0    -e bash -c '../_nsq-bin/nsqlookupd; sleep 5'                                     2>/dev/null &
+konsole --nofork --geometry=1200x300-0+350  -e bash -c '../_nsq-bin/nsqd --lookupd-tcp-address=127.0.0.1:4160; sleep 5'      2>/dev/null &
+konsole --nofork --geometry=1200x300-0+690  -e bash -c '../_nsq-bin/nsqadmin --lookupd-http-address=127.0.0.1:4161; sleep 5' 2>/dev/null &
---
+++ scritps-and-stuff/test_workers.sh
@@ -0,0 +1,4 @@
+
+curl -d '{"email":"oyvindska@gmail.com","result-topic":"verify_result-692963"}' 'http://127.0.0.1:4151/put?topic=verify_requests'
+curl -d '{"email":"adadsad@gmail.com","result-topic":"verify_result-692963"}' 'http://127.0.0.1:4151/put?topic=verify_requests'
+curl -d '{"email":"bill@gmail.com","result-topic":"verify_result-692963"}' 'http://127.0.0.1:4151/put?topic=verify_requests'
